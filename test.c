3c39ed7d7f27f9dcab99b23e71163693ccc345cd
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-06-06 08:19:29 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,77 +1,51 @@
 #define _CRT_SECURE_NO_WARNINGS 1 
-#include"SeqList.h"
-void SeqListTest1()
+#include"Stack.h"
+#include"Queue.h"
+
+void TestStack()
 {
-	SeqList SList;
-	SeqList* pList = &SList;
-	SeqListInit(pList);
-	SeqListPrint(pList);
-	printf("\n------------------\n");
+	Stack st;
+	StackInit(&st);
+	StackPush(&st, 1);
+	StackPush(&st, 2);
 
-	SeqListPushBack(pList, 1);
-	SeqListPushBack(pList, 2);
-	SeqListPushBack(pList, 3);
-	SeqListPushBack(pList, 4);
-	SeqListPushBack(pList, 5);
-	SeqListPrint(pList);
-	printf("\n------------------\n");
+	printf("%d ", StackTop(&st));
+	StackPop(&st);
 
-	SeqListPopBack(pList);
-	SeqListPopBack(pList);
-	SeqListPopBack(pList);
-	SeqListPopBack(pList);
-	//SeqListPopBack(pList);
-	//SeqListPopBack(pList);
-	SeqListPrint(pList);
-	printf("\n------------------\n");
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+	while (StackEmpty(&st) == 0)
+	{
+		printf("%d ", StackTop(&st));
+		StackPop(&st);
+	}
+	StackDestory(&st);
 
-	SeqListPushFront(pList, 10);
-	SeqListPushFront(pList, 11);
-	SeqListPushFront(pList, 12);
-	SeqListPushFront(pList, 13);
-	SeqListPrint(pList);
-	printf("\n------------------\n");
+}
+void TestQueue()
+{
+	Queue q;
+	QueueInit(&q);
+	QueuePush(&q, 1);
+	QueuePush(&q, 2);
 
-	SeqListPopFront(pList);
-	SeqListPopFront(pList);
-	SeqListPopFront(pList);
-	//SeqListPopFront(pList);
-	//SeqListPopFront(pList);
+	printf("%d ", QueueFront(&q));
+	QueuePop(&q);
 
-	SeqListPrint(pList);
-	printf("\n------------------\n");
+	QueuePush(&q, 3);
+	QueuePush(&q, 4);
 
-	
+	while (!QueueEmpty(&q))
+	{
+		printf("%d ", QueueFront(&q));
+		QueuePop(&q);
+	}
+	QueueDestory(&q);
 
-	//int ret = SeqListFind(pList, 1);
-	//if (ret == -1)
-	//{
-	//	printf("Can not Find!\n");
-	//}
-	//else
-	//{
-	//	printf("Find it,index word is %d\n", ret);
-	//}
-
-	SeqListInsert(pList, 1, 33);
-	SeqListInsert(pList, 0, 22);
-	SeqListInsert(pList, 2, 44);
-	SeqListPrint(pList);
-	printf("\n------------------\n");
-
-	SeqListErase(pList, 1);
-	SeqListErase(pList, 2);
-	SeqListErase(pList, 0);
-	SeqListPrint(pList);
-	printf("\n------------------\n");
-
-	SeqListDestory(pList);//´ýÓÅ»¯
 }
 int main()
 {
-	SeqListTest1();
+	//TestStack();
+	TestQueue();
 	return 0;
 }
-
-
-
