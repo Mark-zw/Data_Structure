49bd09d0cb40ccceef08795c268329c5d9e5eaab
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-07-03 14:04:31 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,13 +1,54 @@
 #define _CRT_SECURE_NO_WARNINGS 1 
-#include "SList.h"
+#include"ListNode.h"
+void TestList1()
+{
+	ListNode* phead = ListInit();
+	ListPushBack(phead, 1);
+	ListPushBack(phead, 2);
+	ListPushBack(phead, 3);
+	ListPushBack(phead, 4);
+	ListPushBack(phead, 5);
+	ListPrint(phead);
+	printf("_________________\n");
+	ListPopBack(phead);
+	ListPopBack(phead);
+	ListPopBack(phead);
+	ListPopBack(phead);
+	ListPopBack(phead);
+	//ListPopBack(phead);
+	printf("_________________\n");
+	ListPrint(phead);
+	ListPushFront(phead, 6);
+	ListPushFront(phead, 7);
+	ListPushFront(phead, 8);
+	ListPushFront(phead, 9);
+	ListPrint(phead);
+	printf("_________________\n");
+	ListPopFront(phead);
+	ListPopFront(phead);
+	ListPopFront(phead);
+	ListPrint(phead);
+	printf("_________________\n");
+
+}
+void TestList2()
+{
+	ListNode* phead = ListInit();
+	ListPushBack(phead, 1);
+	ListPushBack(phead, 2);
+	ListPushBack(phead, 3);
+	ListPushBack(phead, 4);
+	ListPushBack(phead, 5);
+
+	ListNode* pos = ListFind(phead, 3);
+	ListInsert(pos, 30);
+	ListPrint(phead);
+	printf("_________________\n");
+}
 int main()
 {
-	SListNode* pList = NULL;//刷题的时候一般就是给一个头指针
-	SListPushBack(&pList, 1);
-	SListPushBack(&pList, 2);
-	SListPushBack(&pList, 3);
-	SListPushBack(&pList, 4);
+	//TestList1();
+	TestList2();
 
-	SListPrint(pList);
 	return 0;
 }
