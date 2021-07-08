83322d60d9d2e8fdd57025f4a24577deef6be742
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-07-08 20:10:57 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,70 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS 1 
-#include"BinaryTree.h"
+#include"Heap.h"
 int main()
 {
-	BTNode* A = CreateNode('A');
-	BTNode* B = CreateNode('B');
-	BTNode* C = CreateNode('C');
-	BTNode* D = CreateNode('D');
-	BTNode* E = CreateNode('E');
-	A->left = B; 
-	A->right = C;
-	B->left = D;
-	B->right = E;
-
-	BinaryTreePrevOrder(A);
-	printf("\n");
-	BinaryTreeInOrder(A);
-	printf("\n");
-	BinaryTreePostOrder(A);
-	printf("\n");
-	printf("TreeSize:%d \n", TreeSize(A));
-
-	printf("TreeLeafSize:%d \n", TreeLeafSize(A));
-	printf("%d\n", BinaryTreeLevelKSize(A, 3));
+	HPDataType arr[] = { 27,15,19,18,28,34,65,49,25,37 };
+	Heap hp;
+	//HeapInit(&hp, arr, sizeof(arr) / sizeof(HPDataType));
+	HeapSort(arr, sizeof(arr) / sizeof(HPDataType));
 	return 0;
 }
-
-//enum Color
-//{
-//	Red,
-//	Green = 3,
-//	Blue
-//};
-//int main()
-//{
-//	enum Color r = Red;
-//	enum Color g = Green;
-//	enum Color b = Blue;
-//
-//	printf("%d\n", r);
-//	printf("%d\n", g);
-//	printf("%d\n", b);
-//
-//	return 0;
-//}
-
-
-//#include <stdio.h>
-//int Sub(int a, int b)
-//{
-//	return (a - b);
-//}
-//main()
-//{
-//	int  x, y, result = 0;
-//	scanf("%d,%d", &x, &y);
-//	result = Sub(x, y);
-//	printf("result = %d\n", result);
-//}
-//#include<stdio.h>
-//main()
-//{
-//	struct stu
-//	{
-//		int num;
-//		char a[5];
-//		float score;
-//	}m = { 1234,"wang",89.5 };
-//	printf(" % d, % s, % f", m.num, m.a, m.score);
-//}
