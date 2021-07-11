54af6dcd4fff2bdc55dfd4561372944fa4d0a6eb
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-07-11 18:57:12 +0800

    Add files via upload

---
+++ c_practice0710/c_homework0710.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include<stdio.h>
+#include<windows.h>
+int main()
+{
+	printf("Hello World!\n");
+	system("pause");
+	return 0;
+}
+
+
+//#include<stdio.h>
+//#define F(X,Y) ((X)+(Y))
+//int main()
+//{
+//	int a = 3, b = 4;
+//	printf("%d\n", F(a++, b++));
+//	return 0;
+//}
+
+//int main()
+//{
+//	char a[20];
+//	char* p1 = (char*)a;
+//	char* p2 = (char*)(a + 5);
+//	int n = p2 - p1;
+//	printf("%d\n", n);
+//	return 0;
+//}
