e3ec84ee41a3180a7a9e9d1c634c1ddc7a26cec3
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-07-26 08:17:36 +0800

    Add files via upload

--- SList.h
+++ SList.h
@@ -1,29 +1,38 @@
-#define _CRT_SECURE_NO_WARNINGS 1 
+#pragma once
 #include<stdio.h>
-#include<assert.h>
 #include<stdlib.h>
 
-typedef int SListDataType;
 
-//结点
-typedef struct SListNode
+//单向 不带头 非循环
+typedef int SLDatatype;
+//定义一个链表
+typedef struct SList
 {
-	SListDataType data;
-	struct SListNode* next;
+	SLDatatype data;
+	struct SList* next;
+}SNode,SList;//SNode强调结点，*SList强调链表
 
-}SListNode;
-
+//初始化
+void SListInit(SList* pList);
+//销毁
+void SListDestory(SList* pList);
 //打印
-void SListPrint(SListNode* phead);
-
-//尾插
-void SListPushBack(SListNode* phead, SListDataType x);
-
-//尾删
-void SListPopBack(SListNode* phead);
-
+void SListPrint(SList** ppList);
+//创建一个节点
+SList* CreateNode(SLDatatype x);
 //头插
-void SListPushFront(SListNode* phead, SListDataType x);
-
+void SListPushFront(SList** ppList, SLDatatype x);
+//尾插
+void SListPushBack(SList** ppList, SLDatatype x);
 //头删
-void SListPushFront(SListNode* phead);
+void SListPopFront(SList** ppList);
+//尾删
+void SListPopBack(SList** ppList);
+//在任意位置插入
+void SListInsert(SList* pList, int pos, SLDatatype x);
+//在任意位置删除
+void SListErase(SList* pList, int pos, SLDatatype x);
+//查找
+int SListSearch(SList* pList, SLDatatype x);
+//修改
+void SListModify(SList* pList, int pos, SLDatatype x);
