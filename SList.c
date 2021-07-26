f710d1e8e024b0e1a5082c4336f0ee77d5ea3466
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-07-26 08:17:44 +0800

    Add files via upload

--- SList.c
+++ SList.c
@@ -1,65 +1,136 @@
-#define _CRT_SECURE_NO_WARNINGS 1 
-#include "SList.h"
-
+#pragma once
+#include"SList.h"
 //打印
-void SListPrint(SListNode* phead)
+void SListPrint(SList** ppList)
 {
-	SListNode* cur = phead;
-	while (cur != NULL)
+	if (*ppList == NULL)
 	{
-		printf("%d ---> ", cur->data);
-		cur = cur->next;//指针指向的地址移动到下一个数据的地址
+		printf("The SList is empty!\n");
 	}
+	SNode* cur = *ppList;
+	while (cur)
+	{
+		if (cur->next != NULL)
+		{
+			printf("%d->", cur->data);
+			cur = cur->next;
+		}
+		if (cur->next == NULL)
+		{
+			printf("%d->NULL", cur->data);
+			cur = cur->next;
+		}
+	}
+	printf("\n");
 }
 
-//封装创建结点
-SListNode* CreateNewNode(SListDataType x)
+//创建一个节点
+SList* CreateNode(SLDatatype x)
 {
-	SListNode* newNode = (SListNode*)malloc(sizeof(SListNode));
-	if (newNode == NULL)
+	SNode* node = (SNode*)malloc(sizeof(SNode));
+	if (node == NULL)
 	{
-		printf("Failed to allocate memory !");
-		exit(-1);
+		perror("CreateNode:");
+		printf("Create a node fail!\n");
+		return NULL;
 	}
-	newNode->data = x;
-	newNode->next = NULL;
-	return newNode;
+	node->data = x;
+	node->next = NULL;
+	return node;
 }
 
 //尾插
-void SListPushBack(SListNode** pphead, SListDataType x)
+void SListPushBack(SList** ppList, SLDatatype x)
 {
-	SListNode* newNode = CreateNewNode(x);
-	if ((*pphead) == NULL)
+	SNode* node = CreateNode(x);
+	//判断链表是否为空
+	if (*ppList == NULL)
 	{
-		*pphead = newNode;
+		//创建一个结点给pList
+		*ppList = node;
 	}
 	else
 	{
-		SListNode* tail = *pphead;
+		//遍历链表找尾部
+		SNode* tail = *ppList;
 		while (tail->next != NULL)
 		{
 			tail = tail->next;
 		}
-		//循环出来后的tail就是NULL，尾指针
-
-		tail->next = newNode;
+		tail->next = node;
 	}
 
-
+	//SNode* cur = (SNode*)malloc(sizeof(SNode));
+	//if (cur == NULL)
+	//{
+	//	perror("SListPushBack:");
+	//	printf("malloc fail!\n");
+	//	return;
+	//}
+	//cur->data = x;
+	//cur->next = NULL;
+	//tail->next = cur;
 }
-
-//尾删
-void SListPopBack(SListNode* phead)
-{
-	//1.空
-	//2.一个结点
-	//3.两个及其以上结点
-
-}
-
 //头插
-void SListPushFront(SListNode* phead, SListDataType x);
-
+void SListPushFront(SList** ppList, SLDatatype x)
+{
+	SList* node = CreateNode(x);
+	node->next = *ppList;
+	*ppList = node;
+}
 //头删
-void SListPushFront(SListNode* phead);
+void SListPopFront(SList** ppList)
+{
+
+}
+//尾删 1.链表为空 2.链表一个结点  3.链表多个结点
+void SListPopBack(SList** ppList)
+{
+	SNode* prev = NULL;
+	SNode* tail = *ppList;
+	if (tail == NULL)
+	{
+		printf("The SList is empty!\n");
+	}
+	else
+	{
+		while (tail->next != NULL)
+		{
+			prev = tail;
+			tail = tail->next;
+		}
+		free(tail);
+		tail = NULL;
+		if (prev != NULL)
+		{
+			prev->next = NULL;
+		}
+	}
+	////判断链表是否为空
+	//if (*ppList == NULL) 
+	//{
+	//	printf("The SList is empty!\n");
+	//}
+	//else
+	//{
+	//	SNode* cur = *ppList;
+	//	if (cur->next == NULL)
+	//	{
+	//		free(cur);
+	//		cur = NULL;
+	//		*ppList = NULL;
+	//	}
+	//	else
+	//	{
+	//		SNode* next = cur->next;
+	//		while (cur->next)
+	//		{
+	//			next = next->next;
+	//			cur = cur->next;
+	//		}
+	//		free(next);
+	//		next = NULL;
+	//		cur->next = NULL;
+	//	}
+	//}
+}
