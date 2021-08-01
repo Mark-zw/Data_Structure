6fda86733258d3f5c04c615dd7a8b02239c90c69
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-08-01 08:54:40 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,54 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS 1 
-#include"DList.h"
-void TestDList1()
+#include"Stack.h"
+void TestStack()
 {
-	DList* pList = DListInit();
-	DListPrint(pList);
-
-	DListPushBack(pList, 0);
-	DListPushBack(pList, 1);
-	DListPushBack(pList, 2);
-	DListPushBack(pList, 3);
-	DListPushBack(pList, 4);
-	DListPushBack(pList, 5);
-	DListPrint(pList);
-	DListPushFront(pList, 1);
-	DListPushFront(pList, 2);
-	DListPushFront(pList, 3);
-	DListPushFront(pList, 4);
-	DListPushFront(pList, 5);
-	DListPrint(pList);
-
-	DListPopBack(pList);
-	DListPopBack(pList);
-	DListPopBack(pList);
-	DListPopBack(pList);
-	DListPopBack(pList);
-	DListPrint(pList);
-
-	int ret = DListEmpty(pList);
-	printf("ret == %d\n", ret);
-	int length = DListLength(pList);
-	printf("length == %d\n", length);
-
-	DListPopFront(pList);
-	DListPopFront(pList);
-	DListPopFront(pList);
-	DListPrint(pList);
-
-	DNode* pos = DListFind(pList, 1);
-	if (pos)
+	Stack st;
+	StackInit(&st);
+	StackPush(&st, 1);
+	printf("%d ", StackTop(&st));
+	StackPop(&st);
+	StackPush(&st, 2);
+	printf("%d ", StackTop(&st));
+	StackPop(&st);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+	while (!StackEmpty(&st))
 	{
-		DListInsert(pos, 40);
+		printf("%d ", StackTop(&st));
+		StackPop(&st);
 	}
-	DListPrint(pList);
 
-	DListDestory(&pList);
-	DListPrint(pList);
-
+	StakDestroy(&st);
 }
+
 int main()
 {
-	TestDList1();
+	TestStack();
 	return 0;
 }
