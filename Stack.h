74dfe22b0180b536412b351de855bf93dd78ad04
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-08-01 08:54:50 +0800

    Add files via upload

--- Stack.h
+++ Stack.h
@@ -1,36 +1,27 @@
-#pragma once
+#define _CRT_SECURE_NO_WARNINGS 1 
 #include<stdio.h>
-#include<assert.h>
 #include<stdlib.h>
-
-////静态的栈
-// struct Stack
-//{
-//	int _a[N];
-//	int _size;
-//};
-
+#include<assert.h>
+#include<stdbool.h>
 typedef int STDataType;
-
-//动态的栈
 typedef struct Stack
 {
-	STDataType* _a;//带_表示为结构体成员，项目工程中经常使用
-	int _top;//栈顶下标
-	int _capacity;
+	STDataType* arr;
+	int top;//栈顶
+	int capacity;//容量
 }Stack;
 
 //初始化
 void StackInit(Stack* pst);
 //销毁
-void StackDestory(Stack* pst);
-//进栈
+void StakDestroy(Stack* pst);
+//入栈
 void StackPush(Stack* pst, STDataType x);
 //出栈
 void StackPop(Stack* pst);
-//栈的元素个数
-int StackSize(Stack* pst);
-//栈状态判断 返回1是空，返回0是非空
-int StackEmpty(Stack* pst);
-//获取栈顶的数据
+//获取栈顶元素
 STDataType StackTop(Stack* pst);
+//检测栈是否为空，空-非零 非空-0
+bool StackEmpty(Stack* pst);
+//获取栈中有效元素的个数
+int StackSize(Stack* pst);
