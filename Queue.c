47035d9263b1a610ae7245662f90c687ecc75d38
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-08-02 11:16:16 +0800

    Add files via upload

--- Queue.c
+++ Queue.c
@@ -1,88 +1,93 @@
 #define _CRT_SECURE_NO_WARNINGS 1 
 #include"Queue.h"
-//初始化
-void  QueueInit(Queue* pq)
+//初始化队列
+void QueueInit(Queue* pq)
 {
 	assert(pq);
-	pq->_head = pq->_tail = NULL;
+	pq->head = pq->tail = NULL;
 }
 //销毁
-void QueueDestory(Queue* pq)
+void QueueDestroy(Queue* pq)
 {
 	assert(pq);
-	QueueNode* cur = pq->_head;
+	QueueNode* cur = pq->head;
 	while (cur)
 	{
-		QueueNode* next = cur->_next;
+		QueueNode* next = cur->next;
 		free(cur);
 		cur = next;
 	}
 }
-//入队列
+//队尾入队列
 void QueuePush(Queue* pq, QDataType x)
 {
 	assert(pq);
 	QueueNode* newnode = (QueueNode*)malloc(sizeof(QueueNode));
 	if (newnode == NULL)
 	{
-		printf("newnode is NULL!\n");
+		printf("malloc fail!\n");
 		exit(-1);
 	}
-	newnode->_data = x;
-	newnode->_next = NULL;
-	if (pq->_head == NULL)
+	newnode->data = x;
+	newnode->next = NULL;
+	if (pq->tail == NULL)
 	{
-		pq->_head = pq->_tail = newnode;
+		pq->head = pq->tail = newnode;
 	}
 	else
 	{
-		pq->_tail->_next = newnode;
-		pq->_tail = newnode;
+		pq->tail->next = newnode;
+		pq->tail = pq->tail->next;
 	}
 }
-//出队列
+//队头出队列
 void QueuePop(Queue* pq)
 {
 	assert(pq);
-	assert(pq->_head);
-	QueueNode* next = pq->_head->_next;
-	free(pq->_head);
-	pq->_head = next;
-	if (pq->_head == NULL)
+	//仅有一个数据
+	if (pq->head->next == NULL)
 	{
-		pq->_tail = NULL;
+		free(pq->head);
+		pq->head = pq->tail = NULL;
+	}
+	else//存在多个数据
+	{
+		QueueNode* next = pq->head->next;
+		free(pq->head);
+		pq->head = next;
 	}
 }
-//获取队列头部数据
+//获取队头元素
 QDataType QueueFront(Queue* pq)
 {
 	assert(pq);
-	assert(pq->_head);
-	return pq->_head->_data;
+	assert(pq->head != NULL);
+	return pq->head->data;
 }
-//获取队列尾部数据
+//获取队尾元素
 QDataType QueueBack(Queue* pq)
 {
 	assert(pq);
-	assert(pq->_head);
-	return pq->_tail->_data;
+	assert(pq->head != NULL);
+	return pq->tail->data;
 }
-//栈状态判断 返回1是空，返回0是非空
-int QueueEmpty(Queue* pq)
-{
-	assert(pq);
-	return pq->_head == NULL ? 1 : 0;
-}
-//队列的元素个数
+//获取队列中有效元素个数
 int QueueSize(Queue* pq)
 {
 	assert(pq);
-	QueueNode* cur = pq->_head;;
-	int size = 0;
-	while (cur != pq->_tail)
+	assert(pq->head != NULL);
+	QueueNode* cur = pq->head;
+	int length = 0;
+	while (cur)
 	{
-		size++;
-		cur = cur->_next;
+		cur = cur->next;
+		length++;
 	}
-	return size;
+	return length;
+}
+//检测队列是否为空，空 --非0 非空--0	
+bool QueueEmpty(Queue* pq)
+{
+	assert(pq);
+	return pq->head == NULL;
 }
