c707408546506e79ff6b77d44512be5195570fb0
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-08-05 11:52:07 +0800

    Add files via upload

--- BinaryTree.h
+++ BinaryTree.h
@@ -1,29 +1,29 @@
-#pragma once
+#define _CRT_SECURE_NO_WARNINGS 1 
 #include<stdio.h>
 #include<stdlib.h>
+
 typedef char BTDataType;
-typedef struct BinaryTreeNode
-{
-	BTDataType _data;
+typedef struct BinaryTreeNode {
+	BTDataType data;
 	struct BinaryTreeNode* left;
 	struct BinaryTreeNode* right;
-
 }BTNode;
 
-//求二叉树结点的个数
-int TreeSize(BTNode* root);
-
-//前序遍历
-void BinaryTreePrevOrder(BTNode* root);
-
-//中序遍历
+//前序
+void BinaryTreePreOrder(BTNode* root);
+//中序
 void BinaryTreeInOrder(BTNode* root);
-
-//后序遍历
+//后序
 void BinaryTreePostOrder(BTNode* root);
-
-//构建伪二叉树
-BTNode* CreateNode(int x);
-
-//二叉树第k层结点个数
+//构建二叉树节点
+BTNode* CreateTreeNode(BTDataType x);
+//统计节点个数
+int TreeSize(BTNode* root);
+//求叶子节点的个数
+int TreeLeafSize(BTNode* root);
+//求第k层节点个数
 int BinaryTreeLevelKSize(BTNode* root, int k);
+//查找值为x的节点
+BTNode* BinaryTreeFind(BTNode* root,BTDataType x);
+//求二叉树的深度
+int BinaryDepth(BTNode* root);
