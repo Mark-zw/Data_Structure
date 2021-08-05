576c29fbd677224e84412f9daa15b56c5d6d5f68
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-08-05 11:51:58 +0800

    Add files via upload

--- BinaryTree.c
+++ BinaryTree.c
@@ -1,88 +1,80 @@
-#define _CRT_SECURE_NO_WARNINGS 1 
+#define _CRT_SECURE_NO_WARNINGS 1
 #include"BinaryTree.h"
-//求二叉树结点的个数
-
-int TreeSize(BTNode* root)
-{
-	//方法一
-	//static int size = 0;
-	//if (root == NULL)
-	//	return 0;
-	//size++;
-	//TreeSize(root->left);
-	//TreeSize(root->right);
-	//return size;
-
-	//方法二
-	if (root == NULL)
-		return 0;
-	else
-		return 1 + TreeSize(root->left) + TreeSize(root->right);
-}
-
-int TreeLeafSize(BTNode* root)
-{
-	if (root == NULL)
-		return 0;
-	else if (root->left == 0 && root->right == 0)
-		return 1;
-	return  TreeLeafSize(root->left) + TreeLeafSize(root->right);
-}
-
-//前序遍历
-void BinaryTreePrevOrder(BTNode* root)
-{
-	if (root == NULL)
-	{
+//前序
+void BinaryTreePreOrder(BTNode* root) {
+	if (root == NULL) {
 		printf("NULL ");
 		return;
 	}
-	printf("%c ", root->_data);
-	BinaryTreePrevOrder(root->left);
-	BinaryTreePrevOrder(root->right);
+	//根   左子树   右子树
+	printf("%c ", root->data);
+	BinaryTreePreOrder(root->left);
+	BinaryTreePreOrder(root->right);
 }
-//中序遍历
-void BinaryTreeInOrder(BTNode* root)
-{
-	if (root == NULL)
-	{
+//中序 左子树 根 右子树
+void BinaryTreeInOrder(BTNode* root) {
+	if (root == NULL) {
 		printf("NULL ");
 		return;
 	}
 	BinaryTreeInOrder(root->left);
-	printf("%c ", root->_data);
+	printf("%c ", root->data);
 	BinaryTreeInOrder(root->right);
 }
-
-//后序遍历
-void BinaryTreePostOrder(BTNode* root)
-{
-	if (root == NULL)
-	{
+//后序 左子树 根 右子树
+void BinaryTreePostOrder(BTNode* root) {
+	if (root == NULL) {
 		printf("NULL ");
 		return;
 	}
 	BinaryTreePostOrder(root->left);
 	BinaryTreePostOrder(root->right);
-	printf("%c ", root->_data);
+	printf("%c ", root->data);
 }
-
-//构建伪二叉树
-BTNode* CreateNode(BTDataType x)
-{
+//构建二叉树节点
+BTNode* CreateTreeNode(BTDataType x) {
 	BTNode* node = (BTNode*)malloc(sizeof(BTNode));
-
-	node->_data = x;
-	node->left = NULL;
-	node->right = NULL;
+	if (node == NULL) {
+		printf("malloc fail!\n");
+		exit(-1);
+	}
+	node->data = x;
+	node->left = node->right = NULL;
 	return node;
 }
-//二叉树第k层结点个数
-int BinaryTreeLevelKSize(BTNode* root, int k)
-{
+//统计节点个数
+int TreeSize(BTNode* root) {
+	//return root == NULL ? 0 : TreeSize(root->left) + TreeSize(root->right) + 1;
 	if (root == NULL)
 		return 0;
-	if (k == 1)
+	else
+		return TreeSize(root->left) + TreeSize(root->right) + 1;
+}
+//求叶子节点的个数
+int TreeLeafSize(BTNode* root) {
+	if (root == NULL)
+		return 0;
+	else if (root->left == NULL && root->right == NULL)
 		return 1;
-	return BinaryTreeLevelKSize(root->left, k - 1) + BinaryTreeLevelKSize(root->right, k - 1);
+	else
+		return TreeLeafSize(root->left) + TreeLeafSize(root->right);
+}
+//求第k层节点个数
+int BinaryTreeLevelKSize(BTNode* root, int k) {
+	if (root == NULL)
+		return 0;
+	else if (k == 1)
+		return 1;
+	else
+		return BinaryTreeLevelKSize(root->left, k - 1) + BinaryTreeLevelKSize(root->right, k - 1);
+}
+//查找值为x的节点
+BTNode* BinaryTreeFind(BTNode* root, BTDataType x);
+//求二叉树的深度
+int BinaryDepth(BTNode* root) {
+	if (root == NULL)
+		return 0;
+	else
+		return BinaryDepth(root->left) > BinaryDepth(root->right) ? 
+		BinaryDepth(root->left) + 1 : BinaryDepth(root->right) + 1;
 }
