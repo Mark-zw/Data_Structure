6c26893c33023f1d6307f4e6b61bf47591639fa2
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-08-08 10:37:15 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,42 +1,12 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include"BinaryTree.h"
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include"Sort.h"
 int main()
 {
-	BTNode* a = CreateTreeNode('A');
-	BTNode* b = CreateTreeNode('B');
-	BTNode* c = CreateTreeNode('C');
-	BTNode* d = CreateTreeNode('D');
-	BTNode* e = CreateTreeNode('E');
-	BTNode* f = CreateTreeNode('F');
-	BTNode* g = CreateTreeNode('F');
-
-	a->left = b;
-	b->left = d;
-	a->right = c;
-	c->left = e;
-	c->right = f;
-
-	b->right = g;
-
-	BinaryTreePreOrder(a);
-	printf("\n");
-	BinaryTreeInOrder(a);
-	printf("\n");
-	BinaryTreePostOrder(a);
-	printf("\n");
-	printf("TreeSize = %d \n", TreeSize(a));
-	printf("TreeLeafSize = %d \n", TreeLeafSize(a));
-	printf("KLevelTreeSize = %d \n", BinaryTreeLevelKSize(a,3));
-	printf("BinaryDepth = %d \n", BinaryDepth(a));
-	BTNode* retArr = BinaryTreeFind(a, 'E');
-	printf("retArr->data = %c\n", retArr->data);
-	BinaryTreeLevelOrder(a);
-	bool ret = BinaryTreeComplete(a);
-	if (ret == true)
-		printf("\nBinaryTreeComplete = 1\n");
-	else
-		printf("\nBinaryTreeComplete = 0\n");
-	BinaryDestroy(&a);
-	a = NULL;
+	int arr[] = { 11,0,2,4,3,1,8,9,10,7,6 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	//InsertSort(arr, n);
+	//ShellSort(arr, n);
+	SelectSort(arr, n);
+	PrintArray(arr, n);
 	return 0;
 }
