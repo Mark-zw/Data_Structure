8f78aec8a6ece4766fc4a928d32e8576f3cdee26
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-08-09 09:01:17 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,12 +1,74 @@
 #define _CRT_SECURE_NO_WARNINGS 1 
 #include"Sort.h"
+//测试排序的性能对比
+void TestOP()
+{
+	srand(time(0));
+	const int N = 100000;
+	int* a1 = (int*)malloc(sizeof(int) * N);
+	int* a2 = (int*)malloc(sizeof(int) * N);
+	int* a3 = (int*)malloc(sizeof(int) * N);
+	int* a4 = (int*)malloc(sizeof(int) * N);
+	int* a5 = (int*)malloc(sizeof(int) * N);
+	int* a6 = (int*)malloc(sizeof(int) * N);
+	for (int i = 0; i < N; ++i)
+	{
+		a1[i] = rand();
+		a2[i] = a1[i];
+		a3[i] = a1[i];
+		a4[i] = a1[i];
+		a5[i] = a1[i];
+		a6[i] = a1[i];
+	}
+	int begin1 = clock();
+	InsertSort(a1, N);
+	int end1 = clock();
+
+	int begin2 = clock();
+	ShellSort(a2, N);
+	int end2 = clock();
+
+	int begin3 = clock();
+	SelectSort(a3, N);
+	int end3 = clock();
+
+	int begin4 = clock();
+	HeapSort(a4, N);
+	int end4 = clock();
+
+	//int begin5 = clock();
+	//QuickSort(a5, 0, N - 1);
+	//int end5 = clock();
+
+	//int begin6 = clock();
+	//MergeSort(a6, N);
+	//int end6 = clock();
+
+	printf("InsertSort:%d\n", end1 - begin1);
+	printf("ShellSort: %d\n", end2 - begin2);
+	printf("SelectSort:%d\n", end3 - begin3);
+	printf("HeapSort: %d\n", end4 - begin4);
+	//printf("QuickSort:%d\n", end5 - begin5);
+	//printf("MergeSort:%d\n", end6 - begin6);
+
+	free(a1);
+	free(a2);
+	free(a3);
+	free(a4);
+	free(a5);
+	free(a6);
+}
 int main()
 {
 	int arr[] = { 11,0,2,4,3,1,8,9,10,7,6 };
 	int n = sizeof(arr) / sizeof(arr[0]);
 	//InsertSort(arr, n);
 	//ShellSort(arr, n);
-	SelectSort(arr, n);
+	//SelectSort(arr, n);
+	//HeapSort(arr, n);
+	BubbleSort(arr, n);
+
+	//TestOP();
 	PrintArray(arr, n);
 	return 0;
 }
