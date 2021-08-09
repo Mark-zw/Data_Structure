f8ca70c491ae0dbb485ff070a3796e8de6438e22
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-08-09 09:01:08 +0800

    Add files via upload

--- Sort.h
+++ Sort.h
@@ -9,3 +9,12 @@ void InsertSort(int* arr, int n);
 void ShellSort(int* arr, int n);
 //选择排序
 void SelectSort(int* arr, int n);
+//堆向下调整  大堆向下调整、小堆向下调整 
+void AdjustDown_small(int* arr, int n, int root);
+void AdjustDown_big(int* arr, int n, int root);
+//堆排序
+void HeapSort(int* arr, int n);
+//冒泡排序
+void BubbleSort(int* arr, int n);
+//快速排序
+void QuickSort(int* arr, int n);
